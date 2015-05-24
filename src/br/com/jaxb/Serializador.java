46e86f22ce9086e4849292d94da6588781a73278
Mateus <mmachado.csp@gmail.com>
2015-05-24 14:52:00 -0300

    Add Classes: Serializador, Deserializador.

---
+++ src/br/com/jaxb/Serializador.java
@@ -0,0 +1,21 @@
+package br.com.jaxb;
+
+import java.io.File;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+public class Serializador {
+
+	public static void main(String[] args) throws JAXBException {
+
+		JAXBContext context = JAXBContext.newInstance(Conta.class);
+		Marshaller marshaller = context.createMarshaller();
+
+		Conta conta = new Conta();
+		conta.setLimite(2000);
+		conta.setSaldo(2000);
+		marshaller.marshal(conta, new File("conta.xml"));
+	}
+}
